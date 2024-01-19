9f43f432d5ca6efa863c62021b3a751080a17daa
Can Kotaman <cankotadamm@gmail.com>
2024-01-19 17:58:49 +0300

    Project Final

--- src/components/CustomerItem/CustomerItem.jsx
+++ src/components/CustomerItem/CustomerItem.jsx
@@ -1,22 +1,36 @@
+// import "./CustomerItem.css";
+
+import styles from "./CustomerItem.module.css"
+
+/* import styled from "styled-components";
+const DeleteButton = styled.button`
+  background-color: red;
+  padding: 8px;
+  transition: opacity 0.5s ease;
+
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+ */
 const CustomerItem = ({ customer, handleDelete }) => {
+
+  console.log(styles);
+
   return (
-    <li className="customer-item">
-      <div className="customer-info">
+    <li className={styles.customerItem}>
+      <div className={styles.customerInfo}>
         <img
           src="https://i.pravatar.cc/300"
           alt=""
-          className="customer-avatar"
+          className={styles.customerAvatar}
         />
-        <span
-          className="customer-name"
-          style={{
-            color: "red",
-          }}
-        >
-          {customer.customerName}
-        </span>
+        <span className={styles.customerName}>{customer.customerName}</span>
       </div>
-      <button className="delete-button" onClick={() => handleDelete(customer)}>
+      <button
+        className={styles.deleteButton}
+        onClick={() => handleDelete(customer)}
+      >
         <i className="bi bi-trash3"></i>
       </button>
     </li>
